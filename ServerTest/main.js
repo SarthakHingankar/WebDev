5faa115d3b994186c5e28de3af730a3658135989
Sarthak <sarthakhingankar@gmail.com>
2024-10-24 23:52:48 +0530

    workning on database

--- ServerTest/main.js
+++ ServerTest/main.js
@@ -1,7 +1,29 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const mysql = require("mysql2");
 const PORT = 3000;
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "Sarthak@17",
+  database: "login",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+const queryDatabase = async (query, params) => {
+  return new Promise((resolve, reject) => {
+    pool.query(query, params, (error, results) => {
+      if (error) {
+        return reject(error);
+      }
+      resolve(results);
+    });
+  });
+};
 
 const server = http.createServer((req, res) => {
   let filePath = path.join(__dirname, req.url === "/" ? "index.html" : req.url);
@@ -47,3 +69,14 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
+
+const runQuery = async () => {
+  try {
+    const results = await queryDatabase("SELECT * FROM users");
+    console.log(results);
+  } catch (error) {
+    console.error("Error executing query:", error);
+  }
+};
+
+runQuery();
