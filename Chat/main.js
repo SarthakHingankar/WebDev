b5bf318e3b08332fdfebfd81c1bf975b1db61114
Sarthak <sarthakhingankar@gmail.com>
2024-12-15 11:22:57 +0530

    trying to work on auth

--- Chat/main.js
+++ Chat/main.js
@@ -1,12 +1,39 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const secret = process.env.secretKey;
 const app = express();
+app.use(express.static(__dirname + "/public"));
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 const PORT = 3000;
 
-app.use(express.static("public"));
+app.get("/", (req, res) => {
+  const token = req.headers["authorization"]?.split(" ")[1];
+  console.log(token);
+  if (!token) {
+    return res.redirect("/login");
+  }
+
+  jwt.verify(token, secret, (err, decode) => {
+    if (err) {
+      return res.redirect("/login");
+    } else {
+      res.sendFile(__dirname + "/public/index.html");
+    }
+  });
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/public/login.html");
+});
+
+app.get("/signup", (req, res) => {
+  res.sendFile(__dirname + "/public/signup.html");
+});
 
 io.on("connection", (socket) => {
   console.log("New client connected");
